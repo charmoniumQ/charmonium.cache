b894d7652ac5d4c090eae7a8bd559cea5064b191
Samuel Grayson <sam@samgrayson.me>
2019-06-24 22:23:22 -0700

    updated script

--- run.sh
+++ run.sh
@@ -1,28 +1,34 @@
 #!/bin/sh
-package=$(python3 -c 'import setuptools; print(setuptools.find_packages()[0])')
+
+namespace=charmonium
+package=${namespace}.$(ls ${namespace} -X | head -n 1)
+
 if [ ! -d env/ ]
 then
     python3 -m virtualenv env/
     . ./env/bin/activate
-    cat `which pip3`
-    cat `pip3 --version`
-    cat `which python3`
-    cat `python3 --version`
     pip3 install -r requirements.txt -r dev-requirements.txt
 fi
+
 . ./env/bin/activate
+
 set -e -x
-[ ! -d .cache/ ] && mkdir .cache/
-modtime=`stat -c '%Y' requirements.txt`-`stat -c '%Y' dev-requirements.txt`
-cache_requirements=.cache/requirements
-if test ! -f "${cache_requirements}" || test "${modtime}" = "$(cat ""${cache_requirements}"")"
-then
-    pip3 install -r dev-requirements.txt -r requirements.txt
-    echo "${modtime}" > "${cache_requirements}"
-fi
+
 # I have chosen to make tests a subdirectory of code so that the next
-# four lines are simpler
-mypy -p ${package}
+# four commands are simpler
+
+# While [--namespace-discovery][1] has been added, "Note that this
+# only affects import discovery – for modules and packages explicitly
+# passed on the command line, mypy still searches for __init__.py[i]
+# files in order to determine the fully-qualified module/package
+# name."
+# [1]: https://mypy.readthedocs.io/en/latest/command_line.html#import-discovery
+touch ${namespace}/__init__.py ; mypy -p ${package} ; rm ${namespace}/__init__.py
+
 python -m pytest  --pyargs ${package}.tests
-pylint ${package}
-scc ${package}
+
+touch ${namespace}/__init__.py ; pylint ${package} --exit-zero ; rm ${namespace}/__init__.py
+
+scc ${namespace}
+
+python3 -c "import ${package}; print(${package}.__doc__)" > ./README.md
