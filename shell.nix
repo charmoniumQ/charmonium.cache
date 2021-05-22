6846fd47e63827c7a2474208f604ed7553cba813
Samuel Grayson <sam@samgrayson.me>
2021-05-22 13:40:34 -0500

    fix: dev: Switched to Nix+Poetry

--- shell.nix
+++ shell.nix
@@ -1,10 +1,27 @@
-# save this as shell.nix
 { pkgs ? import <nixpkgs> {}}:
+
+# pkgs.poetry2nix.mkPoetryEnv {
+#     projectDir = ./.;
+#     # python = pkgs.python310;
+#     # editablePackageSources = {
+#     #   "charmonium.cache" = ./charmonium/cache;
+#     # };
+# }
 
 pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.python310
     pkgs.poetry
     pkgs.nodePackages.pyright
+    pkgs.python
   ];
+  shellHook = ''
+    # create venv if it doesn't exist
+    poetry run true
+
+    export VIRTUAL_ENV=$(poetry env info --path)
+    export POETRY_ACTIVE=1
+    path=$(basename $PWD)
+    source "$VIRTUAL_ENV/bin/activate"
+  '';
 }
